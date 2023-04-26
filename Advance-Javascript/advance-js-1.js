31a37b4eb9176465e8d130261f44271f7e4329cf
Prathamesh <prathameshmane855@gmail.com>
2023-04-26 23:46:14 +0530

    First Lecture of Advance JS

---
+++ Advance-Javascript/advance-js-1.js
@@ -0,0 +1,15 @@
+// Coercion - Type Conversion 
+
+// Whenever we do a operation based on input we can convert the input for operation, we convert type of Input, this conversion can be manually(typecasting(explicit)) done by us or 
+// the language based on certain rules automatically(implicit typecasting/type conversion) convert the types[which is coercion]
+
+
+// Abstract Operations
+// These Operations not part of JS,these are some set of algorithms that are present in the JS Docs but they are not available for usage in JS,
+// We developers cannot use these operation directly, These help the documentaion to understand lot of things internally
+
+// https://262.ecma-international.org/10.0/#sec-abstract-operations
+// 54:30
+// ToNumber - convert the argument to Number 
+// if i/p  is Undefined then NaN
+// if null  is Undefined then +0 
